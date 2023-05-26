f4b9935bfd7ec0d324c7299a5846fc9010a081eb
Todd0554 <toddy9229@outlook.com>
2023-05-26 22:13:08 +1000

    image path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -85,7 +85,7 @@ function NavBar() {
                 <div id="header-carousel" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active" style={{height: "410px"}}>
-                            <img class="img-fluid" src="img/carousel-1.jpg" alt="Image"></img>
+                            <img class="img-fluid" src="/img/carousel-1.jpg" alt="Image"></img>
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style={{maxWidth: "700px"}}>
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3">xxx xxx xxxx xxxxx xxxxx</h4>
@@ -95,7 +95,7 @@ function NavBar() {
                             </div>
                         </div>
                         <div class="carousel-item" style={{height: "410px"}}>
-                            <img class="img-fluid" src="img/carousel-2.jpg" alt="Image"></img>
+                            <img class="img-fluid" src="/img/carousel-2.jpg" alt="Image"></img>
                             <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div class="p-3" style={{maxWidth: "700px"}}>
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3">xxx xxx xxxx xxxxx xxxxx</h4>
